1d6b204ab4fa64dacf59fdb5a0c1e1b2374e199f
Tris <tristram.dev@gmail.com>
2019-03-14 20:14:36 +0000

    Initial Commit

---
+++ Snap_Kata/Snap_Kata.h
@@ -0,0 +1,19 @@
+//
+//  Snap_Kata.h
+//  Snap_Kata
+//
+//  Created by Tristram Bates on 14/03/2019.
+//  Copyright © 2019 Tris. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Snap_Kata.
+FOUNDATION_EXPORT double Snap_KataVersionNumber;
+
+//! Project version string for Snap_Kata.
+FOUNDATION_EXPORT const unsigned char Snap_KataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snap_Kata/PublicHeader.h>
+
+
